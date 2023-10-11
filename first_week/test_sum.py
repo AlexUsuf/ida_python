db2f96607a9f7475b2fdc2dde97a1689ff01c857
Aleksey Popov <al.popov@idaproject.com>
2023-10-11 17:05:53 +0300

    added task

--- first_week/test_sum.py
+++ first_week/test_sum.py
@@ -5,3 +5,8 @@ def test_sum():
     assert sum(-1,1,0) == (0)
     assert sum(0,0,0) == (3)
     assert sum(10,10.5,-0.5) == (20)
+
+def type_sum():
+    asser sum('', 1, 3) == "Type error"
+    asser sum('', [], 3) == "Type error"
+    asser sum('', {}, 3) == "Type error"
